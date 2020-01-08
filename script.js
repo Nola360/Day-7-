2882a3cb1fbe8b347b3f3e727a943f09ac990028
nola360 <adaramolajr@gmail.com>
2020-01-07 23:47:24 -0500

    added& html & js content

--- script.js
+++ script.js
@@ -1,91 +1,76 @@
-// FOR LOOP
-// Declaration of a variable; condition; iteration;
-for (let i = 0; i <= 10; i++) {
-  // console.log('Number ' + i);
+// WINDOW METHODS / OBJECTS / PROPERTIES
 
-  // Break loop
-  if (i === 6) {
-    console.log("Number 6 is my favorite number");
-    continue; // Replaces the number 6 in the iteration with console log message
-  }
-  console.log("Number " + i);
+// console.log(123);
 
-  if (i === 8) {
-    console.log(`Stop the loop at ${i}`);
-    continue;
+// // ALERT - displays alert box in DOM
+// window.alert('Hello World');
 
-  }
-}
+// // PROMPT - displays input box on window (DOM)
+// const input = prompt();
+// alert(input);
 
-//WHILE LOOP - use when un clear on the # of itereations **
-let i = 0;
-while (i < 10) {
-  console.log('Number ' + i);
-  i++;
-  if (i === 8) {
-    console.log(`Number ${i} is the break `);
-    break
-  }
-}
+// //CONFIRM - when deleting something 
+// if (confirm('Are you sure')) {
+//   console.log('Yes');
+// } else {
+//   console.log('No')
+// }
+
+//PROPERTIES 
+
+let val;
 
 
-// DO WHILE - it will run at least once no matter what
-let j = 10;
-do {
-  console.log('Number ' + j);
-  j++;
-  if (j === 15) {
-    console.log(`The loop stops at ${j}`);
-    break;
-  }
-}
-while (j < 20);
+//Outer Height & width
+val = window.outerHeight;
+console.log(val);
+val = window.outerWidth;
+console.log(val);
+
+//SCROLL POINTS - where you are when scrolling
+
+val = window.scrollY;
+console.log(val);
+val = window.scrollX;
+console.log(val);
 
 
-// LOOPING THROUGH ARRAYS - ARRAYS HAVE SPECIFIC METHODS TO USE
-
-const cars = ['BMW', 'Benz', 'Honda', 'Range Rover'];
-
-for (let i = 0; i < cars.length; i++) {
-  // console.log(i);
-  // console.log(cars[i]);
-  console.log(`${cars[i]} is my fav vehicle.`)
-}
-
-//USE FOR EACH LOOP TO LOOP THROUGH AN ARRAY - MUCH CLEANER
-cars.forEach(function (car, index, array) {
-  console.log(`My fav car is a ${index}: ${car}`);
-  console.log(array);
-})
-
-// MAP - used to return a different array
-
-const users = [
-  { id: 1, name: 'Malcolm' },
-  { id: 2, name: 'Martin' },
-  { id: 3, name: 'Patrice' },
-  { id: 4, name: 'Skittles' }
-];
-
-const ids = users.map(function (user) {
-  return user.id
-})
-
-console.log(ids);
-
-// FOR IN LOOP - USED TO LOOP THROUGH OBJECTS
-
-const player = {
-  firstName: 'John',
-  lastName: 'Doe',
-  age: 45
-}
-
-for (let x in player) {
-  console.log(`${x} : ${player[x]}`);
-}
+//LOCATION OBJECT - 
+val = window.location;
+console.log(val);
+val = window.location.hostname;
+console.log(val);
+val = window.location.href;
+console.log(val);
+val = window.location.port;
+console.log(val);
+val = window.location.search;
+console.log(val);
 
 
+// Redirect
+// window.location.href = 'http://google.com';
 
+// Reload
+// window.location.reload();
+// console.log(val);
 
+//HISTORY OBJECT
 
+// window.history.go();
+val = window.history.length;
+console.log(val);
+
+//NAVIGATOR OBJECT - BROWSER BASED
+
+val = window.navigator;
+console.log(val);
+
+val = window.navigator.geolocation;
+console.log(val);
+
+val = window.navigator.vendor;
+console.log(val);
+
+val = window.navigator.productSub;
+console.log(val);
