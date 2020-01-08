00d62cb954aa10c592b110be040beaabc03b44aa
nola360 <adaramolajr@gmail.com>
2020-01-07 23:58:49 -0500

    global and function scope

--- script.js
+++ script.js
@@ -1,76 +1,33 @@
-// WINDOW METHODS / OBJECTS / PROPERTIES
+// Global Scope
+// Let and const have a block level scope, var has a function scope
 
-// console.log(123);
+var a = 1;
+let b = 2;
+const c = 3;
 
-// // ALERT - displays alert box in DOM
-// window.alert('Hello World');
+function test() {
+  var a = 4;
+  let b = 5;
+  const c = 6;
+  console.log('Function Scope: ', a, b, c);
+}
 
-// // PROMPT - displays input box on window (DOM)
-// const input = prompt();
-// alert(input);
+test();
 
-// //CONFIRM - when deleting something 
-// if (confirm('Are you sure')) {
-//   console.log('Yes');
-// } else {
-//   console.log('No')
+
+
+// Block level Scope
+// if (true) {
+//   var a = 4;
+//   let b = 5;
+//   const c = 6;
+//   console.log('If Scope: ', a, b, c);
+
 // }
 
-//PROPERTIES 
 
-let val;
+for (var a = 0; a < 10; a++) {
+  console.log(`Loop: ${a}`)
+}
 
-
-//Outer Height & width
-val = window.outerHeight;
-console.log(val);
-val = window.outerWidth;
-console.log(val);
-
-//SCROLL POINTS - where you are when scrolling
-
-val = window.scrollY;
-console.log(val);
-val = window.scrollX;
-console.log(val);
-
-
-//LOCATION OBJECT - 
-val = window.location;
-console.log(val);
-val = window.location.hostname;
-console.log(val);
-val = window.location.href;
-console.log(val);
-val = window.location.port;
-console.log(val);
-val = window.location.search;
-console.log(val);
-
-
-// Redirect
-// window.location.href = 'http://google.com';
-
-// Reload
-// window.location.reload();
-// console.log(val);
-
-//HISTORY OBJECT
-
-// window.history.go();
-val = window.history.length;
-console.log(val);
-
-//NAVIGATOR OBJECT - BROWSER BASED
-
-val = window.navigator;
-console.log(val);
-
-val = window.navigator.geolocation;
-console.log(val);
-
-val = window.navigator.vendor;
-console.log(val);
-
-val = window.navigator.productSub;
-console.log(val);
+console.log('Global Scope: ', a, b, c);
